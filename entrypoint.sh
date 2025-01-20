ff58dd02741ffc5e349e764caadf118437183d9c
Dmytro Aviedov <dmytro.aviedov@gmail.com>
2025-01-20 16:00:01 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ CODE_PATH="$1"
 cd "${CODE_PATH}"
 
 echo "123 ${ACCESS_TOKEN}"
-nslookup git.avedov.info
+git ls-remote https://${ACCESS_TOKEN}@git.avedov.com/adsoft/water-valve.git
 
 git config --global init.defaultBranch master
 idf.py reconfigure  
